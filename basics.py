7e1adf3665d0d88f4469e45154a5dd8cd7e9d893
rkorzen <korzeniewski@gmail.com>
2021-10-14 19:48:57 +0200

    Zadanie 1 i 2

---
+++ basics.py
@@ -0,0 +1,31 @@
+# int
+x = 1
+y = 2
+
+print(x + y)
+
+# float
+# complex
+# bool
+# str
+
+print(1)
+print("123")
+print(x)
+
+# ctrl + /
+# imie = input("Podaj imie: ")
+# print(imie)
+
+# liczba = input("Podaj liczbe:")
+# liczba = int(liczba)
+
+liczba = int(input("Podaj liczbe:"))
+print(liczba)
+print(liczba+10)
+
+# 1 Pobierz od uzytkownika imie i wzrost i wypisz w terminalu w nastepujacy sposob:
+# Imie: Antek
+# Wzrost: 198
+
+# 2 Zapytaj o dlugosc podstawy i wysokosc trojkata i wypisz wyliczone pole
---
+++ pierwszy_program.py
@@ -0,0 +1,22 @@
+# to jest komentarz
+
+"""
+Tutaj moglby byc opis programu
+
+..
+..
+
+
+"""
+
+x = 1  # wspolrzedna x
+y = 2
+print("Hello World!")
+
+# zad_1.py
+# zad 1. napisz program, ktory wyliczy pole trojkata o podstawie 10 i wysokosci 5
+# pole trojkata: 25
+# uruchomoc program
+# python zad_1.py
+
+# Zad 2. pola kola o promieniu 7 - analogicznie
