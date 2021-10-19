d95c9779e7f9e40f7d57c7623384e67f8e9d552c
rkorzen <korzeniewski@gmail.com>
2021-10-19 19:23:13 +0200

    zajecia #3

---
+++ Dzien03/bmi.py
@@ -0,0 +1,40 @@
+"""
+
+ Skrypt obliczający BMI
+
+ BMI = waga (kg) / wzrost (m^2)
+ Uwaga! - wzrost podajemy w cm
+"""
+weight = input("Podaj wagę w kg:")
+height = input("Podaj wzrost w cm:")
+
+weight = int(weight) # konwertujemy z napisu na int
+height = int(height) # konwertujemy z napisu na int
+
+# liczenie formuły
+# bmi = weight / pow( height/100 , 2)
+# print(bmi)
+
+bmi = weight / (height/100)**2
+#print("Twoje BMI = "+str( round(bmi,2) ))
+print(f"Twoje BMI = {bmi:.2f}")
+
+# import math
+# bmi = weight / math.pow(  height/100 , 2 )
+# print(bmi)
+
+"""
+<18.5 - zjedz coś
+>=18.5 i <25 - ok
+>= 25 - ogranicz kebaby
+"""
+
+if bmi>=25:
+    print("ogranicz kebaby")
+    print("ogranicz monsterki")
+elif bmi<18.5:
+    print("zjedz coś")
+else:
+    print("OK")
+
+#print(weight, height)
