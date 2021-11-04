85e0d3b18ba317d66cec6e72d67b54294f5fce62
rkorzen <korzeniewski@gmail.com>
2021-11-04 19:24:12 +0100

    dzien 8

--- Dzien07/funkcje_rekurencja.py
+++ Dzien07/funkcje_rekurencja.py
@@ -1,6 +1,8 @@
 """
  Rekurencja i funkcje
 """
+import time
+
 def silnia_iter(n): # silnia iteracyjne
     wynik = 1
     for x in range(1, n+1):
@@ -16,3 +18,17 @@ def silnia_rek(n): # silnia rekurencyjnie
         return 1
 
 print(silnia_iter(5), silnia_rek(5))
+
+LICZBA_ITER = 10_000 # liczba iteracji
+N = 300 # liczba dla ktorej licze silnie
+
+# badanie szybkosci wykonania funkcji
+t1 = time.time() # zwraca tzw. unix timestamp w formie liczby float
+for _ in range(LICZBA_ITER): silnia_rek(N)
+t2 = time.time()
+print(f"Czas wykonania silnia_rek = {t2-t1} sek.")
+
+t1 = time.time() # zwraca tzw. unix timestamp w formie liczby float
+for _ in range(LICZBA_ITER): silnia_iter(N)
+t2 = time.time()
+print(f"Czas wykonania silnia_iter = {t2-t1} sek.")
